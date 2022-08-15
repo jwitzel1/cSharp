782022b3073e3a1ae13e06aea705b288d73a49ef
jwitzel1 <jason.s.witzel@gmail.com>
2022-08-15 04:23:06 -0700

    consoleApp

---
+++ CSharp_BasicProjects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+{ 
+    Console.WriteLine("Welcome to Acme Accounting Systems");
+    Console.WriteLine("We're \"Accounting\" on you!");
+    Console.ReadLine();
+}
