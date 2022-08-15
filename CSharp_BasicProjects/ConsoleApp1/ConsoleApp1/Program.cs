19b87355cbac8751295251e6ab2f52570cb396be
jwitzel1 <jason.s.witzel@gmail.com>
2022-08-15 12:09:19 -0700

    updated

--- CSharp_BasicProjects/ConsoleApp1/ConsoleApp1/Program.cs
+++ CSharp_BasicProjects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-{ 
-    Console.WriteLine("Welcome to Acme Accounting Systems");
-    Console.WriteLine("We're \"Accounting\" on you!");
-    Console.ReadLine();
+using System;
+
+namespace HelloAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("We're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
 }
