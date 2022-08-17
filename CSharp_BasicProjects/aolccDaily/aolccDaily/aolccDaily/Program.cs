c9aac8497272b7bf50ba767f4cf25989bea2cfd8
jwitzel1 <jason.s.witzel@gmail.com>
2022-08-17 16:58:36 -0700

    aolccDaily

---
+++ CSharp_BasicProjects/aolccDaily/aolccDaily/aolccDaily/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aolcc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("_____________________");
+
+            Console.WriteLine("What course are you in?");
+            string myCourseName = Console.ReadLine();
+
+            Console.WriteLine("What page number ?");
+            string pageNum = Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string needHelp = Console.ReadLine();
+            bool needsHelp = Convert.ToBoolean(needHelp);
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string myHoursStudied = Console.ReadLine();
+            byte numValHrsStudied = Convert.ToByte(myHoursStudied);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
