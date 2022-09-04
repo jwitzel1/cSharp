799f0626d055174c3940275f150643bdd4a74b39
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-04 15:08:49 -0700

    anotherClassAss

---
+++ CSharp_BasicProjects/anotherClassMethodAss/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classNmethod5
+{
+    public class doMathStuff4
+    {
+        //method in
+        public void doStuff4(int x)
+        {
+            int y = x / 2;
+            Console.WriteLine("in: " + x + "out: " + y + ".");
+            Console.ReadLine();
+        }
+
+        public int doStuff5(int x)
+        {
+            int y = x * 1000;
+            return y;
+            
+        }
+
+        public int doStuff5(string x)
+        {
+
+            int y = -1;
+            return y;
+
+        }
+
+    }
+
+    public static class doMathStuff6
+    {
+        static void giveMeAHunny()
+        {
+            Console.WriteLine("100");
+        }
+    }
+}
---
+++ CSharp_BasicProjects/anotherClassMethodAss/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classNmethod5
+{
+    class Program
+    {
+        //main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number. We will Havle it.");
+            string myNum = Console.ReadLine();
+            int x = Convert.ToInt32(myNum); int a;
+            doMathStuff4 dm4 = new doMathStuff4();
+            dm4.doStuff4(x);
+            Console.WriteLine("Press enter to continue.");
+            Console.WriteLine("Now can you give me a number?");
+            string rslt = Console.ReadLine();
+            doMathStuff4 dm5 = new doMathStuff4();
+            int i = 0;
+            bool result=int.TryParse(rslt, out i);
+            if (i != 0)
+            {
+                int z = Convert.ToInt32(rslt);
+                a=(dm5.doStuff5(z));
+                Console.WriteLine("result: " + a + ".");
+            }
+            else
+            {
+                string z = Console.ReadLine();
+                a = (dm5.doStuff5(z));
+                Console.WriteLine("result: " + a + ".");
+            }
+            
+
+        
+        }
+    }
+}
