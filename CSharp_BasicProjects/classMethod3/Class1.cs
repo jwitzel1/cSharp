b94b579182ae371ec6b8a25e3e7b4ac4ecdb3e1d
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-04 14:09:14 -0700

    classMethodAss3

---
+++ CSharp_BasicProjects/classMethod3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classNmethod3
+{
+    public class doMathStuff3
+    {
+        //method in
+        public void doStuff3(int x, int y)
+        {
+            int numDo = x * y;
+            Console.WriteLine("a x b is " + numDo + " because b is " + y);
+        }
+    }
+}
---
+++ CSharp_BasicProjects/classMethod3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classNmethod3
+{
+    class Program
+    {
+        //main
+        static void Main(string[] args)
+        {
+            doMathStuff3 dm = new doMathStuff3();
+             
+            dm.doStuff3(34, 22);
+            Console.WriteLine("hit enter. watch as we specify parameter by name.");
+            Console.ReadLine();
+            dm.doStuff3(x: 35, y: 23);
+        }
+    }
+}
