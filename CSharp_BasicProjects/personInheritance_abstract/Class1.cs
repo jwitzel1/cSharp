a689de686711362a16d2a7709b62e848ec5b707b
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-06 11:39:33 -0700

    abstract inheritance

---
+++ CSharp_BasicProjects/personInheritance_abstract/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personInheritance
+{
+    public abstract class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        //method SayName
+        public static void SayName() //need method use
+        {
+         
+        }
+    }
+   
+}
---
+++ CSharp_BasicProjects/personInheritance_abstract/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personInheritance
+{
+    public class Employee:Person
+    {
+        public int EmpID { get; set; }
+
+        public  void SayName() //actual method use
+        {
+            var fullName = FName + " " + LName + ".";
+            Console.WriteLine("Name: " + fullName);
+        }
+    }
+    
+}
