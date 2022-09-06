43a2847e420ae9a946574e29744884dbc4ff971d
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-06 11:41:13 -0700

    21

--- CSharp_BasicProjects/oneAndTwenty/Class1.cs
+++ CSharp_BasicProjects/oneAndTwenty/Class1.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace oneAndTwenty
 {
-    public class Game
+    public abstract class Game
     {
         public List<string> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
+    
+        public abstract void Play();    
 
-        public void ListPlayers()
+        public virtual void ListPlayers()
         {
             foreach (string player in Players)
             {
--- CSharp_BasicProjects/oneAndTwenty/Program.cs
+++ CSharp_BasicProjects/oneAndTwenty/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace oneAndTwenty
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            TwentyOneGame game = new TwentyOneGame();
+            game.ListPlayers = new List<string>() { "p1", "p2", "p3" };
+            game.ListPlayers();
+            Console.ReadLine();
 
             Deck deck = new Deck();
             deck.Shuffle(3);          
