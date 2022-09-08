2efe7fc969d7811145d155ec675fbd77a3d3b3d6
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-08 07:17:39 -0700

    enums

---
+++ CSharp_BasicProjects/Enums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    enum DaysOfWeek
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+    static void Main(string[] args)
+    {
+        string DaysOfWeekEntered;
+        Console.WriteLine("Enter the day of the week in full");
+       
+
+        try
+        {
+            DaysOfWeekEntered = Console.ReadLine();
+            DaysOfWeek dayEntered = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), DaysOfWeekEntered);
+            Console.WriteLine("You picked " + dayEntered);
+            Console.ReadLine();
+        }
+        catch
+        {
+            Console.WriteLine("That is not a valid day of the week. Try again.");
+            Console.WriteLine("Enter the day of the week in full");
+            DaysOfWeekEntered = Console.ReadLine();
+            DaysOfWeek dayEntered = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), DaysOfWeekEntered);
+            Console.WriteLine("You picked " + dayEntered);
+            Console.ReadLine();
+        }
+        finally
+        {
+            Console.WriteLine("Days of week are: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+            Console.WriteLine("Enter the day of the week in full");
+            DaysOfWeekEntered = Console.ReadLine();
+            DaysOfWeek dayEntered = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), DaysOfWeekEntered);
+            Console.WriteLine("You picked " + dayEntered);
+            Console.ReadLine();
+        }
+    }
+}
