3a6fc61f15bf439d2e21c4f9ac2811c585de01c8
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-23 02:49:13 -0700

    exceptions

---
+++ CSharp_BasicProjects/exceptions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findExceptions
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool good = false;
+            int age = 0;
+            while (!good)
+            {
+                
+                try
+                {
+                    Console.WriteLine("NOTE: You must be born to access this site. \n Unborn access to this site is unauthorized.  Rude computer asks, HOW OLD ARE YOU?\n ");
+                    age = Convert.ToInt32(Console.ReadLine());
+                    if (age <= 0)
+                    {
+                        
+                        Console.WriteLine("** You are unauthorized to be here, unborn! ** ");
+                        Console.ReadLine();
+                        throw new UnauthorizedAccessException();
+
+                    }
+                    int yearBorn = DateTime.Now.Year - age;
+                    Console.WriteLine("You were born in the year of our lord {0}.",yearBorn);
+                    Console.ReadLine();
+                    good=true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("An emergency is going on. I think those were LETTERS, or decimals. We want ONLY numbers.");
+                    Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An emergency is still going on.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+         
+
+
+}
