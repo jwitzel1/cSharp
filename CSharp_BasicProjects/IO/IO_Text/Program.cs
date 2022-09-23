b7b7fc4c41e273f6d2544c241d18944e692ccadd
jwitzel1 <jason.s.witzel@gmail.com>
2022-09-22 18:41:35 -0700

    otherstuff

--- CSharp_BasicProjects/IO/IO_Text/Program.cs
+++ CSharp_BasicProjects/IO/IO_Text/Program.cs
@@ -4,45 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IO
+namespace OtherStuff
 {
+    public class stringMaker
+    {
+        public stringMaker(string a) : this(a, ".")
+        {
+
+        }
+        public stringMaker(string a, string b)
+        {
+            string first = a.ToUpper();
+            string second = b;
+            string last = ".";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-          //  Console.Write("Echo what number:  ");
-           // string echoThis = Console.ReadLine();
-            //File.WriteAllText(@"D:\_jasons_stuff\_fullStack_school\CSharp\cSharp\CSharp_Projects\textDump\log.txt", echoThis);
-           // string echoed=File.ReadAllText(@"D:\_jasons_stuff\_fullStack_school\CSharp\cSharp\CSharp_Projects\textDump\log.txt");
-           // Console.WriteLine("echo is:  "+ echoed);
-
-            DateTime dateTime = new DateTime(1995, 5, 23, 8, 32, 45);
-           // Console.WriteLine(dateTime);
-            DateTime yearOfBirth = new DateTime(1995, 5, 23, 8, 32, 45);
-            DateTime yearOfGrad = new DateTime(2020, 6, 1, 16, 34, 22);
-
-         //   TimeSpan = yearOfGrad - yearOfBirth;
-          //  Console.WriteLine(TimeSpan + " Days between " + yearOfGrad + " and " + yearOfBirth );
-           // Console ReadLine();
-
-              Console.Write("Enter Hours:  ");
-            int hrs = Convert.ToInt32(Console.ReadLine());
-
-            DateTime now =  DateTime.Now;
-            DateTime dtime=now.AddHours(hrs);
-          //  Console.Write(dtime);
-
-
-         // Console.WriteLine(now);
-         
-
-            Console.WriteLine("It is now "+ now + "\n\n In " + hrs + " hours it will be: \n\n" + dtime + ":" + now.Minute);
-            Console.ReadLine();
-           
-            Console.ReadLine();
-
-        
+            const string userName = "Dude";
+            string text0 = "i";
+            string text1 = " am jason";
             
+
+            stringMaker myString = new stringMaker(text0,text1);
+            var  fullString = "I am jason";
+            stringMaker myString2 = new stringMaker(fullString);
+
+
+            //DateTime dateTime = new DateTime(1995, 5, 23, 8, 32, 45);
+
+            //DateTime yearOfBirth = new DateTime(1995, 5, 23, 8, 32, 45);
+            //DateTime yearOfGrad = new DateTime(2020, 6, 1, 16, 34, 22);
+
+            //Console.Write("Enter Hours:  ");
+            //int hrs = Convert.ToInt32(Console.ReadLine());
+
+            //DateTime now =  DateTime.Now;
+            //DateTime dtime=now.AddHours(hrs);
+
+            //Console.WriteLine("It is now "+ now + "\n\n In " + hrs + " hours it will be: \n\n" + dtime + ":" + now.Minute);
+            //Console.ReadLine();
+
         }
     }
 }
---
+++ CSharp_BasicProjects/otherStuff/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherStuff
+{
+    public class stringMaker
+    {
+        public stringMaker(string a) : this(a, ".")
+        {
+
+        }
+        public stringMaker(string a, string b)
+        {
+            string first = a.ToUpper();
+            string second = b;
+            string last = ".";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string userName = "Dude";
+            string text0 = "i";
+            string text1 = " am jason";
+
+
+            stringMaker myString = new stringMaker(text0, text1);
+            var fullString = "I am jason";
+            stringMaker myString2 = new stringMaker(fullString);
+
+
+        }
+    }
+}
